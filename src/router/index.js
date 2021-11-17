c1ed7435a1f4d4882260a5c1047ffe2259b6fe58
Mike Miller <mikemille999@gmail.com>
2021-11-16 23:38:12 -0600

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import PlaceIndex from "../views/PlaceIndex.vue";
+import PlaceNew from "../views/PlaceNew.vue";
+import PlaceShow from "../views/PlacesShow.vue";
+import PlacesEdit from "../views/PlacesEdit.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +22,10 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  { path: "/places", name: "Index", component: PlaceIndex },
+  { path: "/newplaces", name: "PlaceNew", component: PlaceNew },
+  { path: "/places/:id", name: "PlaceShow", component: PlaceShow },
+  { path: "/places/:id/edit", name: "PlacesEdit", component: PlacesEdit },
 ];
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="places-new">
+      <form v-on:submit.prevent="updatePlace()">
+        <h1>update place</h1>
+        <div>
+          <label>title:</label>
+          <input type="text" v-model="currentPlaceParams.title" />
+        </div>
+        <div>
+          <label>body:</label>
+          <input type="text" v-model="currentPlaceParams.body" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+      <button v-on:click="destroyPlace()">delete</button>
+    </div>
+    <div>
+      <p>Name</p>
+      <input type="text" v-model="newPlace.title" />
+      <p>Adress</p>
+      <input type="text" v-model="newPlace.body" />
+      <button v-on:click="makePlace">Add place</button>
+    </div>
+    <div v-for="place in places" :key="place.id">
+      <h3>{{ place.name }}</h3>
+      <p>{{ place.adress }}</p>
+      <router-link v-bind:to="`/places/${place.id}`">
+        <a href="#" class="btn btn-primary">See More</a>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      places: [],
+      currentPlaceParams: {},
+      errors: [],
+      place: {},
+      newPlace: {},
+    };
+  },
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("/places").then((response) => {
+        this.places = response.data;
+        console.log(this.places);
+      });
+    },
+    makePlace: function () {
+      axios
+        .place("/places", this.newPlace)
+        .then(() => {
+          this.$router.push("/places");
+        })
+        .catch((error) => console.log(error.response));
+    },
+    updatePlace: function () {
+      axios
+        .patch(`/places/${this.$route.params.id}`, this.currentPlaceParams)
+        .then((response) => {
+          this.$router.push(`/places/${response.data.id}`);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    destroyPlace: function () {
+      axios.delete(`places/${this.$route.params.id}`).then((response) => {
+        console.log(response.data);
+        this.$router.push("/places");
+      });
+    },
   },
 };
 </script>
